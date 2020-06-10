791db6eb15fb6d15d751340eacfd1ab1d1724eff
lam <lam@chelsea.com>
2020-06-10 21:37:41 +0800

    create branch

--- src/main/java/com/Test.java
+++ src/main/java/com/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("update 1");
+        System.out.println("create branch");
     }
 }
